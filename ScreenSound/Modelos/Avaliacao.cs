bf308f6cce77990d6292c94babc056d9131e3f97
laborba <lucasadeborba@gmail.com>
2024-02-20 15:15:16 -0300

    Retirando o switch

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ScreenSound.Modelos;
 
-namespace ScreenSound.Modelos
+internal class Avaliacao
 {
-    internal class Class1
+    public int Nota { get; }
+
+    public Avaliacao(int nota)
     {
+        Nota = nota;
     }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+
 }
+
